f0832af4b6cacf6636570af8599a1a82aac3748f
Saulo Costa <saulotarsobc@gmail.com>
2023-04-06 22:44:02 -0300

    :fire:

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const remoteMain = require("@electron/remote/main");
+const remoteMain = require("@electron/remote/main").initialize();
 
 let main;
-remoteMain.initialize();
 
 const createWindow = () => {
-  main = new BrowserWindow({
-    // width: 360,
-    // minWidth: 360,
-    // maxWidth: 360,
-    // height: 500,
-    // frame: false,
-    // autoHideMenuBar: true,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
-  main.loadFile(path.join(__dirname, './view/index.html'));
-  // main.webContents.openDevTools();
-  remoteMain.enable(main.webContents);
+    main = new BrowserWindow({
+        // width: 360,
+        // minWidth: 360,
+        // maxWidth: 360,
+        // height: 500,
+        // frame: false,
+        // autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    main.loadFile(path.join(__dirname, './view/index.html'));
+    main.setTitle(`Electron My Boilerplate`);
+    // main.webContents.openDevTools();
+    remoteMain.enable(main.webContents);
 };
 
 app.on('ready', () => {
-  createWindow();
+    createWindow();
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
+
 /* code */
